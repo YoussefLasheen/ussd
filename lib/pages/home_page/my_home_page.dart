72a49c4609ca99c8d5ab78abf2acb13b45fad58e
Youssef Lasheen <youssef@lasheen.dev>
2023-04-02 20:25:22 +0200

    Add the app color for flavours

--- lib/pages/home_page/my_home_page.dart
+++ lib/pages/home_page/my_home_page.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:ussd/flavors.dart';
+import 'package:ussd/pages/home_page/widgets/code_card.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         title: Text(F.title),
       ),
       body: Center(
-        child: Text(
-          'Hello ${F.title}',
-        ),
+        child: ListView.builder(
+          itemCount: F.code.length,
+          itemBuilder: (context, index) {
+            return CodeCard(
+              code: F.code[index],
+            );
+          },
+        )
       ),
     );
   }
