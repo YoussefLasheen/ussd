cf482a0e6af67eb57043ada3e9159eba46574777
Youssef Lasheen <youssef@lasheen.dev>
2023-04-02 20:58:10 +0200

    Set directionality to rtl

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'flavors.dart';
 import 'pages/home_page/my_home_page.dart';
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: MaterialApp(
-        title: F.title,
-        theme: ThemeData.light(useMaterial3: true).copyWith(
-          colorScheme: ColorScheme.light(
-            primary: F.color,
-          ),
+    return MaterialApp(
+      title: F.title,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.light(
+          primary: F.color,
         ),
-        home: MyHomePage(),
       ),
+      home: const Directionality(
+          textDirection: TextDirection.rtl, child: MyHomePage()),
     );
   }
 }
